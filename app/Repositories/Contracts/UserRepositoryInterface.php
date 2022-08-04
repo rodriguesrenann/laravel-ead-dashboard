d85c3a11894f144c34f0307e6c07e5d6522239a0
Renan <renanrodrigues_cb_@hotmail.com>
2022-08-04 15:49:14 -0300

    Implementado repository de user

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -2,10 +2,12 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface UserRepositoryInterface
 {
-    public function getAll(): array;
-    public function findById(string $id): object;
+    public function getAll(): Collection;
+    public function findById(string $id): ?object;
     public function create(array $data): object;
     public function update(string $id, array $data): object;
     public function delete(string $id): bool;
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use App\Repositories\Contracts\UserRepositoryInterface;
 
 class UserRepository implements UserRepositoryInterface
@@ -15,28 +16,28 @@ class UserRepository implements UserRepositoryInterface
         $this->model = $model;
     }
 
-    public function getAll(): array
+    public function getAll(): Collection
     {
-
+        return $this->model->get();
     }
 
-    public function findById(string $id): object
+    public function findById(string $id): ?object
     {
-
+        return $this->model->find($id);
     }
 
     public function create(array $data): object
     {
-
+        return $this->model->create($data);
     }
 
     public function update(string $id, array $data): object
     {
-
+        return $this->model->where('id', $id)->update($data);
     }
 
     public function delete(string $id): bool
     {
-
+        return $this->model->where('id', $id)->delete();
     }
 }
