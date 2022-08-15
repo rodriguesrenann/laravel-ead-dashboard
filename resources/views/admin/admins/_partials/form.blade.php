a36d7db47b7ee2bceabaf5810bad31f183d35cf3
Renan <renanrodrigues_cb_@hotmail.com>
2022-08-15 17:20:18 -0300

    Correção view admin

--- resources/views/admin/admins/_partials/form.blade.php
+++ resources/views/admin/admins/_partials/form.blade.php
@@ -2,12 +2,12 @@
 <div class="">
     <label class="block text-sm text-gray-600" for="name">Nome</label>
     <input class="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded" id="name" name="name" type="text" required
-        placeholder="Nome" aria-label="Name" value="{{$user->name ?? old('name')}}">
+        placeholder="Nome" aria-label="Name" value="{{$admin->name ?? old('name')}}">
 </div>
 <div class="mt-2">
     <label class="block text-sm text-gray-600" for="email">E-mail</label>
     <input class="w-full px-5  py-2 text-gray-700 bg-gray-200 rounded" id="email" name="email" type="email"
-        required placeholder="E-mail" aria-label="Email" value="{{$user->email ?? old('name')}}">
+        required placeholder="E-mail" aria-label="Email" value="{{$admin->email ?? old('name')}}">
 </div>
 <div class="mt-2">
     <label class=" block text-sm text-gray-600" for="password">Senha</label>
